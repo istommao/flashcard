780f2e9116f52996c53b4be213c9b67f72ab5a87
istommao <istommao@gmail.com>
2017-06-02 20:19:26 +0800

    Add card app and model

--- server/card/models.py
+++ server/card/models.py
@@ -1,3 +1,21 @@
+"""card models"""
 from django.db import models
 
-# Create your models here.
+from extension.modelutil import RandomFixedCharField
+
+
+class Card(models.Model):
+    """Card model."""
+    uid = RandomFixedCharField('编号', max_length=16, unique=True)
+    name = models.CharField('名称', max_length=32)
+
+    creation_time = models.DateTimeField('创建时间', auto_now_add=True)
+    updation_time = models.DateTimeField('更新时间', auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        """Meta."""
+        verbose_name = '卡片管理'
+        verbose_name_plural = '卡片管理'
